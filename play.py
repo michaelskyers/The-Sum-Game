9fbde9160596de1eb55bc4b8e5b7ade7ef82013b
michaelskyers <michaelskyers85@gmail.com>
2024-04-04 15:51:04 +0100

    completed game settings and refined power-ups

--- play.py
+++ play.py
@@ -24,7 +24,14 @@ def welcome(target = 25, choices = 3):
 
 def main_menu(target = 25, choices = 3, player = 1):
     print(""+
-          "\nGame Menu"+
+          "\n##########################"+
+          "\nCurrent Settings"+
+          "\n##########################"+
+          f"\nChoice Range: 1 to {choices}"+
+          f"\nTarget Total: {target}")
+
+    print(""+
+          "\n>>>>>>>>Game Menu<<<<<<<<<"+
           "\n1. Player vs Player"+
           "\n2. Player vs AI"+
           "\n3. Game Settings"+
@@ -38,7 +45,7 @@ def main_menu(target = 25, choices = 3, player = 1):
         case 2:
             ai_gameloop(target, choices)
         case 3:
-            game_settings()
+            game_settings(target, choices)
         case 4:
             print("Thank you for playing!")
 
@@ -67,7 +74,7 @@ def gameloop(target, choices, player):
     if play_again:
         gameloop(target, choices, 1)
     else:
-        main_menu()
+        main_menu(target, choices, player)
 
 def ai_gameloop(target, choices):
     """
@@ -112,30 +119,46 @@ def ai_gameloop(target, choices):
         "Depletion Burst": "Player is able to subtract one more than the choice limit"
         }
 
-    def activate_ability(ability, ability_dict, choices):
-
-        a_list = list(ability_dict.keys())
+    def activate_ability(ability, choices, is_ai, total):
         
         match (ability):
-            case "Time Warp":#a_list[0]:
-                print("You have skipped your turn")
+            case "Time Warp":
+                if is_ai:
+                    print("The AI has skipped its turn.")
+                else:
+                    print("You have skipped your turn.")
+                
                 time.sleep(2)
                 return 0
             
-            case "Double Strike":#a_list[1]:
+            case "Double Strike":
+                if is_ai:
+                    number = ai_choice(0, 2, total, choices)
+                    time.sleep(1)
+                    print("1st number selected")
+                    
+                    total += number
+
+                    number += ai_choice(0, 2, total, choices)
+                    time.sleep(1)
+                    print("2nd number selected")
+
+                    return number
+                
                 number = int(valid_input("Please select your first number: ",1,choices))
                 number += int(valid_input("Please select your second number: ",1,choices))
                 return number
             
-            case "Surge Overflow":#a_list[2]:
-                print("You have chosen to add", choices + 1)
+            case "Surge Overflow":
+                print("Amount to be added is", choices + 1)
                 return choices + 1
             
-            case "Depletion Burst":#a_list[3]:
-                print("You have chosen to subtract", choices + 1)
+            case "Depletion Burst":
+                print("Amount to be subtracted is", choices + 1)
                 return -(choices + 1)
 
     player_power = choice(list(power_ups.keys()))
+    ai_power = choice(list(power_ups.keys()))
 
     while total < target:
         print()
@@ -145,15 +168,15 @@ def ai_gameloop(target, choices):
             if player_power != "":
                 print(f"Special ability available\n-> {player_power}: {power_ups[player_power]}")
 
-                use_ability = valid_input("\nWould you like to use your ability?"+
-                             "\n1. Yes"+
-                             "\n2. No\n> ",1,2) == "1"
+                use_ability = int(valid_input(f"\nHow much would you like to add to the total? (Number between 1 and {choices} (inclusive)"+
+                                              "\nEnter 0 if you would like to use your ability"+
+                                              "\n> ",0,choices)) == 0
 
                 if use_ability:
-                    number = activate_ability(player_power, power_ups, choices)
+                    number = activate_ability(player_power, choices, False, total)
                     player_power = ""
                 else:
-                    number = int(valid_input(f"How much would you like to add to the total? (Number between 1 and {choices} (inclusive)\n> ",1,choices))
+                    number = use_ability
                 
             else:
                 print("No ability available")
@@ -161,8 +184,20 @@ def ai_gameloop(target, choices):
             
         else:
             print(ai+"\nThinking...")
-            number = ai_choice(wait, difficulty, total, choices)
-            print("The AI has chosen", number)
+            time.sleep(2)
+
+            if ai_power != "":
+                if total % (choices + 1) == 0 and total > target // 2:
+                    print(f"The AI has used {ai_power}!")
+                    number = activate_ability(ai_power, choices, True, total)
+                    ai_power = ""
+                else:
+                    number = ai_choice(wait, difficulty, total, choices)
+                    print("The AI has chosen", number)
+                    
+            else:
+                number = ai_choice(wait, difficulty, total, choices)
+                print("The AI has chosen", number)
             
         total += number
         print("TOTAL:",total)
@@ -170,9 +205,12 @@ def ai_gameloop(target, choices):
 
         if total >= target:
             if not player_turn:
-                print("Well done! That is a victory for humanity!")
+                to_string = "Well done! That is a victory for humanity!"
             else:
-                print("Unfortunately AI is one step closer to world domination. You lost.")
+                to_string = "Unfortunately AI is one step closer to world domination. You lost."
+
+            border = "#" * len(to_string)
+            print(border,to_string,border, sep = "\n")
                 
         player_turn = not player_turn
 
@@ -183,15 +221,57 @@ def ai_gameloop(target, choices):
     if play_again:
         ai_gameloop(target, choices)
     else:
-        main_menu()
+        main_menu(target, choices, 1)
         
 
-def game_settings():
+def game_settings(target, choices):
     """
     Change target total and number of choices
     """
-    # Maybe call main_menu again with new settings
-    pass
+    
+    print(""+
+          "\nCurrent Settings"+
+          "\n##########################"+
+          f"\nChoice Range: 1 to {choices}"+
+          f"\nTarget Total: {target}\n")
+
+    update_settings = int(valid_input(
+        """
+        Would you like to change these settings?
+        1. Yes
+        2. No
+        > """,1,2)) == 1
+
+    if update_settings:
+        settings = [
+            [2, 16],
+            [3, 25],
+            [3, 45],
+            [5, 31],
+            [5, 55],
+            [8, 64]
+            ]
+
+        print(""+
+              "\n\t    Available Settings"+
+              "\n\t    ------------------"+
+              "\n\t    | Range | Target |"+
+              "\n\t    ------------------"
+              )
+        
+        for i in range(len(settings)):
+            print(f"\t{i + 1}.  |   {settings[i][0]}   |   {settings[i][1]}   |"+
+                  "\n\t    ------------------")
+
+        new_setting = int(valid_input("Select a new setting: ",1,len(settings)))
+        new_setting = settings[new_setting - 1]
+        choices = new_setting[0]
+        target = new_setting[1]
+
+    print("Returning to main menu...")
+    time.sleep(2)
+    
+    main_menu(target, choices, 1)
 
 def play():
     """
@@ -202,3 +282,6 @@ def play():
 
 # Starts the game
 play()
+
+# other power_up ideas
+# force opponent to randomly pick three, shuffle combinations, skip combinations, reverse, allow to pick power ups, guess what will pick, traps
